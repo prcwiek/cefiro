37d069036fbadd8b75ed4be011692665d9a2c863
Pawel Cwiek <prc.altodato@gmail.com>
2025-09-27 16:02:07 +0200

    added start_date and end_date

--- R/c_shear.R
+++ R/c_shear.R
@@ -6,13 +6,17 @@
 #' or a numeric vector.
 #' @param ws_signals character, name(s) of wind speed signal(s). If NULL, the main\cr
 #' signal is used.
+#' @param start_date character; start date in the format YYYY-MM-DD.
+#' @param end_date character; end date in the format YYYY-MM-DD.
 #' @param numeric_directions logical, if TRUE directions as numbers,\cr
 #' otherwise as directions names
 #'
 #' @importFrom magrittr %>%
 #'
 #' @export
-c_shear <- function(cx = NULL, ws_signals = NULL, numeric_directions = TRUE) {
+c_shear <- function(cx = NULL, ws_signals = NULL,
+                    start_date = NULL, end_date = NULL,
+                    numeric_directions = TRUE) {
   # check if cx is c_mseries object
   if (!is_c_mseries(cx))  {
     stop("cefiro package error: Invalid input format! Argument is not c_mseries object or a vector.",
@@ -27,6 +31,21 @@ c_shear <- function(cx = NULL, ws_signals = NULL, numeric_directions = TRUE) {
   if (length(ws_signals) > 2) {
     stop("cefiro package error: Invalid input! Too many wind speed signals provided.",
          call. = FALSE)
+  }
+  # check if a start date and an end date are in the period range of cx
+  if (!is.null(start_date)) {
+    if (as.POSIXct(start_date, tz = "UTC") < cx$start_date |
+        as.POSIXct(start_date, tz = "UTC") > cx$end_date) {
+      stop("cefiro package error: Invalid input! Start date out of range.",
+           call. = FALSE)
+    }
+  }
+  if (!is.null(end_date)) {
+    if (as.POSIXct(end_date, tz = "UTC") > cx$end_date |
+        as.POSIXct(end_date, tz = "UTC") < cx$start_date) {
+      stop("cefiro package error: Invalid input! End date out of range.",
+           call. = FALSE)
+    }
   }
 
   # define shear function\
@@ -46,15 +65,29 @@ c_shear <- function(cx = NULL, ws_signals = NULL, numeric_directions = TRUE) {
     rm(ws_temp)
   }
 
+  # getting signlas names
   ws1_n <- names(cx$wind_speed[ws_signals[1]])
   ws2_n <- names(cx$wind_speed[ws_signals[2]])
-  ws1_data <- as.numeric(cx$mdata[, ws1_n])
-  ws2_data <- as.numeric(cx$mdata[, ws2_n])
-
-  # get information about main direction signal
   main_dir_n <- as.character(cx$main_wind_dir)
   main_dir_h <- as.numeric(cx$wind_dir[cx$main_wind_dir])
-  main_dir_data <- as.numeric(cx$mdata[, main_dir_n])
+
+  # filter period
+  if (is.null(start_date) & is.null(end_date)) {
+    cx <- cx$mdata
+  } else if(!is.null(start_date) & is.null(end_date)) {
+    cx <- cx$mdata[paste0(start_date, "/")]
+  } else if(is.null(start_date) & !is.null(end_date)) {
+    cx <- cx$mdata[paste0("/", end_date)]
+  } else if (!is.null(start_date) & !is.null(end_date)) {
+    cx <- cx$mdata[paste(start_date, end_date, sep = "/")]
+  }
+
+  # get wind spped signals data
+  ws1_data <- as.numeric(cx[, ws1_n])
+  ws2_data <- as.numeric(cx[, ws2_n])
+
+  # get main direction signal data
+  main_dir_data <- as.numeric(cx[, main_dir_n])
 
   # create a data set for calculations
   shear_input <- data.frame(ws1_data, ws2_data, main_dir_data) %>%
@@ -62,7 +95,7 @@ c_shear <- function(cx = NULL, ws_signals = NULL, numeric_directions = TRUE) {
 
 
   # initialize input vectors for .C call
-  count_in <- rep(0,16)   # count of records in each of 16 sectors, from 0 to 337.5
+  records_in <- rep(0,16)   # count of records in each of 16 sectors, from 0 to 337.5
   shear_in <- rep(0,16)   # average shear values in each secotr
   wsl_in <- rep(0,16)     # average wind speed values at  the lower level in each sector
   wsh_in <- rep(0,16)     # average wind speed values at  the lower level in each sector
@@ -74,13 +107,13 @@ c_shear <- function(cx = NULL, ws_signals = NULL, numeric_directions = TRUE) {
              ws1 = as.double(shear_input$ws1_data), ws2 = as.double(shear_input$ws2_data),
              dir = as.double(shear_input$main_dir_data),
              hl = as.double(ws1_h), hh = as.double(ws2_h),
-             count = as.double(count_in),
+             records = as.double(records_in),
              wsl = as.double(wsl_in),
              wsh = as.double(wsh_in),
              shear = as.double(shear_in))
 
   dout <- tibble::tibble(sector = round(seq(from = 0, to = 337.5, by = 22.5) ,1),
-                         count = round(resp$count, 0),
+                         records = round(resp$records, 0),
                          ws1 = round(resp$wsl, 3),
                          ws2 = round(resp$wsh, 3),
                          shear = resp$shear)
